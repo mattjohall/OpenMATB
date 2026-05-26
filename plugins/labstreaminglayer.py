8a87f3ecd8db37e2846eab9410b91b97dd08be05
mjhall44 <36865308+mattjohall@users.noreply.github.com>
2026-05-26 17:38:34 -0400

    LSL autostart recording

--- plugins/labstreaminglayer.py
+++ plugins/labstreaminglayer.py
@@ -8,6 +8,8 @@ from typing import Any, Callable
 
 from core import validation
 from plugins import Instructions
+import socket
+from time import sleep
 
 try:
     import pylsl
@@ -24,14 +26,19 @@ class Labstreaminglayer(Instructions):
             "streamsession": validation.is_boolean,
             "pauseatstart": validation.is_boolean,
             "state": validation.is_string,
+            "socketnum": validation.is_string,
+            "LSLfilename": validation.is_string,
+            "recordinglocation": validation.is_string,
         }
 
-        self.parameters.update({"marker": "", "streamsession": False, "pauseatstart": False})
+        self.parameters.update({"marker": "", "streamsession": False, "pauseatstart": False, "socketnum": 22345, "LSLfilename": "", "recordinglocation": "False"})
 
         self.stream_info: Any | None = None
         self.stream_outlet: Any | None = None
         self.stop_on_end: bool = False
-
+        self.outlet: bool = False
+        self.state: bool = False
+        self.createPort()
         self.lsl_wait_msg: str = _("Please enable the OpenMATB stream into your LabRecorder.")
 
     def start(self) -> None:
@@ -54,7 +61,6 @@ class Labstreaminglayer(Instructions):
 
     def update(self, dt: float) -> None:
         super().update(dt)
-
         if self.parameters["streamsession"] is True and self.logger.lsl is None:
             self.logger.lsl = self
         elif self.parameters["streamsession"] is False and self.logger.lsl is not None:
@@ -78,6 +84,42 @@ class Labstreaminglayer(Instructions):
         super().stop()
         self.stream_info = None
         self.stream_outlet = None
+        if self.state:
+            if self.outlet:
+                s = self.socket
+                s.sendall(b"stop\n")
+                print('stopped LSL')
+            self.state = False
 
     def get_msg_slide_content(self, str_msg: str) -> str:
         return f"<title>Lab streaming layer\n{self.lsl_wait_msg}"
+    
+    def createPort(self):
+        try:
+            self.socket = socket.create_connection(("localhost", self.parameters['socketnum']))
+            self.outlet = True
+            print('Successfully sees LSL Lab Recorder')
+        except:
+            print('Could not bind LSL Lab Recorder, no recording.')    
+    def startLSL(self) -> None:
+            if self.outlet:
+                s = self.socket
+                s.sendall(b"update\n")
+                s.sendall(b"select all\n")
+                sleep(0.2)
+                if self.parameters["LSLfilename"] != "":
+                    command = f"filename {{root:{self.parameters['recordinglocation']}}} {{template:{self.parameters['LSLfilename']}}}\n"
+                    print(f'Sending LSL setup {command}')
+                    s.sendall(command.encode('utf-8'))
+                    sleep(0.2)
+                sleep(0.2)
+                s.sendall(b"start\n")
+                print('started LSL')
+                self.state = True
+    def stopLSL(self) -> None:
+        if self.state:
+            if self.outlet:
+                s = self.socket
+                s.sendall(b"stop\n")
+                print('stopped LSL')
+            self.state = False
